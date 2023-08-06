4612e30dab76a16aa772e0f1168577c963a4a91a
pedrojaraujo <pedroa990@gmail.com>
2023-08-06 18:34:07 -0300

    starting project

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Banner = () => {
+  return (
+  <header className='banner__container'>
+    <img src="./images/banner.png" alt="banner__header" />
+  </header>
+  )
+}
+
+export default Banner
---
+++ src/components/TextField/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.css"
+import BackgroundForm from './BackgroundForm'
+
+
+const Form = () => {
+  return (
+    <BackgroundForm />
+)
+  
+}
+
+export default Form
