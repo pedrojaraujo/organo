2b41c2f79c13c00a16bfe64c63025eb115253b5b
pedrojaraujo <pedroa990@gmail.com>
2023-08-28 16:28:12 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,37 @@ function App() {
     {
       id: 1,
       name: "Programação",
-      cor: "#d9f7e9",
+      cor: "#57c278",
     },
     {
       id: 2,
       name: "Front-End",
-      cor: "#e8f8ff",
+      cor: "#82cffa",
     },
     {
       id: 3,
       name: "Data Science",
-      cor: "#f0f8e2",
+      cor: "#a6d157",
     },
     {
       id: 4,
       name: "Devops",
-      cor: "#fde7e8",
+      cor: "#ff6d6a",
     },
     {
       id: 5,
       name: "Ux e Design",
-      cor: "#fae9f5",
+      cor: "#ffa0e7",
     },
     {
       id: 6,
       name: "Mobile",
-      cor: "#fff5d9",
+      cor: "#ffba05",
     },
     {
       id: 7,
       name: "Inovação e Gestão",
-      cor: "#ffeedf",
+      cor: "#ff8a29",
     },
   ])
 
@@ -47,53 +47,11 @@ function App() {
     {
       id: 1,
       name: "Pedro Joaquim Araujo",
-      post: "Programador Front-End",
+      post: "Programador Full Stack",
       image: "https://github.com/pedrojaraujo.png",
       time: "Programação",
     },
-    {
-      id: 2,
-      name: "Marcelo",
-      post: "Data Scientist",
-      image:
-        "https://image-resize.qconcursos.com/?image=https%3A%2F%2Fassets.atlas.grupoq.io%2Finstructors%2F39289c1d-11e1-4298-a95a-cbf39646518b.png&fit=scale-down&width=576&quality=75",
-      time: "Data Science",
-    },
-    {
-      id: 3,
-      name: "Pedro ",
-      post: "Back-End",
-      image: "https://github.com/pedrojaraujo.png",
-      time: "Programação",
-    },
-    {
-      id: 4,
-      name: "Pedro Joaquim Araujo",
-      post: "Front-End Developer",
-      image: "https://github.com/pedrojaraujo.png",
-      time: "Front-End",
-    },
-    {
-      id: 5,
-      name: "Pedro Joaquim Araujo",
-      post: "Devops",
-      image: "https://github.com/pedrojaraujo.png",
-      time: "Devops",
-    },
-    {
-      id: 6,
-      name: "Pedro Joaquim Araujo",
-      post: "Mobile Engineer",
-      image: "https://github.com/pedrojaraujo.png",
-      time: "Mobile",
-    },
-    {
-      id: 7,
-      name: "Pedro Joaquim Araujo",
-      post: "Designer",
-      image: "https://github.com/pedrojaraujo.png",
-      time: "Ux e Design",
-    },
+
   ]);
 
   const newEmployee = (employee) => {
